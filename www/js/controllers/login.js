c1331b4d6fdc9e1842fc21b1c9bc818532d677da
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-06-05 16:46:20 +0530

    login with mobile ready

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -42,8 +42,11 @@ angular.module('appskeleton')
                 $window.location.reload();
                 $state.go("app.main");
             }
+            else if(data.data.message==="conflict"){
+                $scope.result="Please specify country code if using Mobile number";
+            }
             else if(data.data.message==="fail"){
-                $scope.result="Wrong email or password";
+                $scope.result="Wrong Email/Username/Mobile or password";
             }
             else{
                 $scope.result="Error occurred! Try again later.";
